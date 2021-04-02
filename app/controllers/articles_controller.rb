e9cdf497382168e4e169932c240b80e62756610c
Josema Sar <65446320+josemasar@users.noreply.github.com>
2021-04-03 00:59:05 +0200

    devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,37 @@ class ArticlesController < ApplicationController
     end
 
     def create
-      @article = Article.new(title: params[:article][:title], body:params[:article][:body])
+      @article = Article.new(articles_params)
       if @article.save
         redirect_to @article
       else
         render :new
       end
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(articles_params)
+        redirect_to @article
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def articles_params
+      params.require(:article).permit(:title, :body)
     end
 
   end
